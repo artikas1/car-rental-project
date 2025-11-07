c6f3c07f808d84f8a8886baae68deb6ccdb3e29d
Artiom <artas.ple@gmail.com>
2025-11-08 03:02:03 +0900

    Initial commit

---
+++ car-rental/src/test/java/com/artiom/car/CarRentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artiom.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
