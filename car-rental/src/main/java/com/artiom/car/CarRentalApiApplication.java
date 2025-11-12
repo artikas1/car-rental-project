6cd5ed261a7c19b91f153e641b65a7a1e6b073a7
Artiom <artas.ple@gmail.com>
2025-11-12 20:37:08 +0900

    Reconfigured projects
    Added User, Token and Role entities and repositories

--- car-rental/src/main/java/com/artiom/car/CarRentalApiApplication.java
+++ car-rental/src/main/java/com/artiom/car/CarRentalApiApplication.java
@@ -2,8 +2,10 @@ package com.artiom.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CarRentalApiApplication {
 
 	public static void main(String[] args) {
