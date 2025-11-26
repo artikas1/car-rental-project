0766f52655f0926f97427954ed1dfb67477869b1
Artiom <artas.ple@gmail.com>
2025-11-26 21:43:46 +0900

    added initial JWT security configuration

---
+++ car-rental/src/main/java/com/artiom/car/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.artiom.car.security;
+
+import com.artiom.car.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override //ensure the method is implemented from the interface
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
